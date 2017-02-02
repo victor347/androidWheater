5a2a870a51f55b08af1047b63ef2aa98b76f30fa
victor ortegon <victor347@gmail.com>
2017-02-01 19:30:23 -0500

    Linear Layouts and weights

--- app/src/main/java/com/vicsoft/wheater/wheater/MainActivity.java
+++ app/src/main/java/com/vicsoft/wheater/wheater/MainActivity.java
@@ -1,13 +1,37 @@
 package com.vicsoft.wheater.wheater;
 
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String TAG = MainActivity.class.getCanonicalName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v(TAG, "Pasando por onCreate");
+
+        Button changeSystemButton = (Button) findViewById(R.id.changeSystem);
+
+        changeSystemButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Log.v(TAG, "Boton Pulsado");
+
+        ImageView systemImage = (ImageView) findViewById(R.id.systemImage);
+
+        systemImage.setImageResource(R.drawable.offline_weather2);
+
     }
 }
