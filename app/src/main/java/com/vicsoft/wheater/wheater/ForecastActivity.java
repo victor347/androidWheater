008a91e357b57a268275ba726a14ee4f20501c90
victor ortegon <victor347@gmail.com>
2017-02-05 23:56:49 -0500

    cambio de pantalla y salvar preferencias

--- app/src/main/java/com/vicsoft/wheater/wheater/ForecastActivity.java
+++ app/src/main/java/com/vicsoft/wheater/wheater/ForecastActivity.java
@@ -1,8 +1,13 @@
 package com.vicsoft.wheater.wheater;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,12 +15,16 @@ import android.widget.TextView;
 public class ForecastActivity extends AppCompatActivity {
 
     public static final String TAG = ForecastActivity.class.getCanonicalName();
+    private static final int REQUEST_UNITS = 235;
+    public static final String PREFERENCE_SHOW_CELSIUS = "showCelsius";
 
     private TextView mMaxTemp;
     private TextView mMinTemp;
     private TextView mHumidity;
     private TextView mDescription;
     private ImageView mForecastImage;
+    private boolean mShowCelsius;
+    private Forecast mForecast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +33,10 @@ public class ForecastActivity extends AppCompatActivity {
 
         Log.v(TAG, "Pasando por onCreate");
 
-        Forecast forecast =  new Forecast(30.2f, 15.6f, 25, "Cielo despejado", R.drawable.ico_02);
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        mShowCelsius = pref.getBoolean(PREFERENCE_SHOW_CELSIUS, true);
+
+        Forecast forecast = new Forecast(30.2f, 15.6f, 25, "Cielo despejado", R.drawable.ico_02);
 
         mMaxTemp = (TextView) findViewById(R.id.tempMax);
         mMinTemp = (TextView) findViewById(R.id.tempMin);
@@ -36,14 +48,91 @@ public class ForecastActivity extends AppCompatActivity {
     }
 
     private void setForecast(Forecast forecast) {
-        mMaxTemp.setText(String.format(getString(R.string.tempMaxFormat),forecast.getMaxTemp()));
-        mMinTemp.setText(String.format(getString(R.string.tempMinFormat),forecast.getMinTemp()));
-        mHumidity.setText(String.format(getString(R.string.humidityFormat),forecast.getHumidity()));
+
+        float maxTemp = forecast.getMaxTemp();
+        float minTemp = forecast.getMinTemp();
+        String units = "°C";
+
+        if(!mShowCelsius){
+
+            maxTemp = toFahrenheit(maxTemp);
+            minTemp = toFahrenheit(minTemp);
+            units = "°F" ;
+        }
+
+        mMaxTemp.setText(String.format(getString(R.string.tempMaxFormat), maxTemp, units));
+        mMinTemp.setText(String.format(getString(R.string.tempMinFormat), minTemp, units));
+        mHumidity.setText(String.format(getString(R.string.humidityFormat), forecast.getHumidity()));
         mDescription.setText(String.valueOf(forecast.getDescription()));
         mForecastImage.setImageResource(forecast.getIcon());
+
+        mForecast = forecast;
     }
 
+    private float toFahrenheit(float temp) {
 
+        return (temp * 1.8f)+32;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.settings, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        boolean superReturn = super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.show_settings) {
+
+            Intent intent = new Intent(this, SettingsActivity.class);
+
+            if(mShowCelsius){
+                intent.putExtra(SettingsActivity.EXTRA_UNITS, R.id.rBCelcius);
+            }
+            else{
+                intent.putExtra(SettingsActivity.EXTRA_UNITS, R.id.rBFarenheit);
+            }
+
+            //startActivity(intent);
+            startActivityForResult(intent, REQUEST_UNITS);
+            return true;
+        }
+
+        return superReturn;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_UNITS) {
+            if (resultCode == RESULT_OK) {
+
+                int optionSelected = data.getIntExtra(SettingsActivity.EXTRA_UNITS, R.id.rBFarenheit);
+
+                if (optionSelected == R.id.rBFarenheit) {
+
+                    mShowCelsius =false;
+
+                } else if (optionSelected == R.id.rBCelcius) {
+
+                    mShowCelsius =true;
+                }
+
+                setForecast(mForecast);
+
+                PreferenceManager.getDefaultSharedPreferences(this).edit().putBoolean(PREFERENCE_SHOW_CELSIUS, mShowCelsius).apply();
+
+            } else if (resultCode == RESULT_CANCELED) {
+
+            }
+        }
+    }
 }
 
 
--- app/src/main/java/com/vicsoft/wheater/wheater/SettingsActivity.java
+++ app/src/main/java/com/vicsoft/wheater/wheater/SettingsActivity.java
@@ -1,11 +1,15 @@
 package com.vicsoft.wheater.wheater;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    public static final String EXTRA_UNITS = "units";
 
     private RadioGroup mRadioGroup;
 
@@ -15,6 +19,8 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
         mRadioGroup = (RadioGroup) findViewById(R.id.rGUnits);
+
+        ((RadioButton)findViewById(getIntent().getIntExtra(EXTRA_UNITS, R.id.rBCelcius))).setChecked(true);
 
         findViewById(R.id.btnOk).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,9 +41,15 @@ public class SettingsActivity extends AppCompatActivity {
 
     public void acceptSettins(){
 
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra(EXTRA_UNITS , mRadioGroup.getCheckedRadioButtonId());
+        setResult(RESULT_OK, returnIntent);
+        finish();
     }
 
     public void cancelSettings(){
 
+        setResult(RESULT_CANCELED);
+        finish();
     }
 }
